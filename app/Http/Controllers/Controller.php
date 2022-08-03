9bade70e2fd4591a9768e5c14ccf85d43ea69beb
sydney2323 <sydneykb38@gmail.com>
2022-08-03 12:54:21 +0300

    updated

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,6 +7,10 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+     /**
+     * @OA\Info(title="iPF Fund request App", version="1")
+     */
+
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
